10518bea4f7631dc1ee2ebac8f615dca918aa556
VIVIN KRISHNAN <vivin@FFDCM.local>
2015-11-24 23:40:38 +0530

    Identation

--- MultiLineNotifications/apps/MultiLineNotifications/android/native/src/com/MultiLineNotifications/GCMIntentService.java
+++ MultiLineNotifications/apps/MultiLineNotifications/android/native/src/com/MultiLineNotifications/GCMIntentService.java
@@ -67,9 +67,12 @@ public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentSer
  			
  		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);		
  		
-		Notification.Builder notificationBuilder = new Notification.Builder(
-				this).setSmallIcon(icon).setTicker(ticker).setWhen(when)
-				.setContentTitle(title).setContentText(msg)
+		Notification.Builder notificationBuilder = new Notification.Builder(this)
+                .setSmallIcon(icon)
+                .setTicker(ticker)
+                .setWhen(when)
+				.setContentTitle(title)
+                .setContentText(msg)
 				.setStyle(new Notification.BigTextStyle().bigText(msg))
 				.setContentIntent(pendingIntent);
  		
